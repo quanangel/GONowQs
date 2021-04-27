aa49aff04b24172c57aea8382be5ecbf742022e3
Qs <quanangel@outlook.com>
2021-04-28 01:37:08 +0800

    edit-router

--- http/routers/router.go
+++ http/routers/router.go
@@ -1,6 +1,8 @@
 package routers
 
 import (
+	"net/http"
+	"nowqs/frame/config"
 	admin "nowqs/frame/http/admin/routers"
 
 	"github.com/gin-gonic/gin"
@@ -8,13 +10,22 @@ import (
 
 // NewRouter is router function
 func NewRouter(r *gin.Engine) *gin.Engine {
-	r.StaticFile("/swagger", "./docs/swagger.json")
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
 
+	// admin routers
 	r = admin.NewRouters(r)
+
+	// swagger router group
+	swagger := r.Group("/swagger")
+	{
+		adminPath := config.GetHttpPath() + config.PathSeparator + "admin"
+		swagger.StaticFS("/admin", http.Dir(config.GetHttpPath()+config.PathSeparator+"assets"+config.PathSeparator+"swagger-ui"))
+		swagger.StaticFile("/admin.json", adminPath+config.PathSeparator+"swagger"+config.PathSeparator+"swagger.json")
+	}
 	return r
 }
